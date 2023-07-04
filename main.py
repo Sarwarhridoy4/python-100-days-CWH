1405bdf597ed0e68a4d98a547d1977be4417c7cc
Sarwarhridoy4 <ssarwarhridoy4@gmail.com>
2023-07-04 14:38:41 +0600

    fifth day

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ print('Hellow World...')
 print('Wellcome Back..')
 print(5)
 print('Buy..')
+
+#practicing comment
+print('I am a python programmer')
+
+# New Line
+print('I am a python \n programmer')
+'''Multiline comments'''
