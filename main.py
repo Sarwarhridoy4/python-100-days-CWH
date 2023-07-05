b98464de71f60f15bbff35f7fd12904638f40d6b
Sarwarhridoy4 <ssarwarhridoy4@gmail.com>
2023-07-05 10:10:23 +0600

    type-casting done

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ print('I am a python programmer')
 # New Line
 print('I am a python \n programmer')
 '''Multiline comments'''
+
+
+
+'''TypeCasting'''
+
+a='25'
+b='45'
+
+print('The result will be Without typecasting',(a+b))
+print('The result will be With typecasting',(int(a)+int(b)))
+print('Type of a will be',(type(a)))
+print('Type of b will be',(type(b)))
